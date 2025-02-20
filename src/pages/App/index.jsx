7a806bb3ccb9e9b83625a87b8e915e1647f0f910
Mikes <riak1826@gmail.com>
2025-02-20 09:58:33 -0500

    vamos bien con esto chicos

---
+++ src/pages/App/index.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex w-[100%] h-[100vh] bg-slate-300'>
+        <h1 className='text-3xl'>
+          Hola Mundo que mas bien o no?   que hay de nuevo viejo
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
