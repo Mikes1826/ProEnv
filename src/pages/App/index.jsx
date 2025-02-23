44c6f0c3be7510e2b75ba5b0abd6e254c48f90de
Mikes <riak1826@gmail.com>
2025-02-23 10:03:47 -0500

    Creacion de routas

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,15 +1,33 @@
 
+import { useRoutes, BrowserRouter } from  "react-router-dom"
+import { Home } from "../Home"
+import { SignIn } from "../SignIn"
+import { SignUp } from "../SignUp"
+import { Profile } from "../Profile"
+import { NotFound } from "../NotFound"
 import './App.css'
 
-function App() {
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    {path: "/", element: <Home/>},
+    {path: "/sign-in", element: <SignIn/>},
+    {path: "/sign-up", element: <SignUp/>},
+    {path: "/profile", element: <Profile/>},
+    {path: "/*", element: <NotFound/>}
+  ])
+  return routes
+}
+
+const App = () => {
 
   return (
     <>
-      <div className='flex w-[100%] h-[100vh] bg-slate-300'>
-        <h1 className='text-3xl'>
-          Hola Mundo que mas bien o no?   que hay de nuevo viejo
-        </h1>
-      </div>
+      <BrowserRouter>
+        <AppRoutes>
+          
+        </AppRoutes>
+      </BrowserRouter>
     </>
   )
 }
