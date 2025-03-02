a7a8144b75d063fce85c909044eb959e4b3e9527
juanpablotb05 <jtabaresbedoya@gmail.com>
2025-03-02 15:10:29 -0500

    actuañizacion de 404

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,16 @@
-import { Layout } from "../../components/Layout"
+    import { Layout } from "../../components/Layout"
+    import background from "../../assets/ENVIFO.png";
 
-const Home = () => {
-    return (
-<Layout>
-    <p>ayuda</p>
-</Layout>
-    )
-}
+    const Home = () => {
+        return (
+    <Layout>
+        <div classname="flex flex-col items-center justify-center main-h-screem bg-white">
+            <div classname="mb-8 flex flex-col items-center">
+            <img src={background} alt="Background" className="w-40 h-40" />
+            </div>
+        </div>
+    </Layout>
+        )
+    }
 
 export { Home }
--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,17 +1,13 @@
 import { Layout } from "../../components/Layout";
 
-
 const NotFound = () => {
-    return (
-        <Layout>
-            <div className=" flex flex-1 flex-col items-center justify-center">
-            <div class="relative flex item-center justify-center w-64 h-64 border-4 border-black rounded-full">
-                <h1 className=" absolute justify-center item-center font-bold">404</h1>
-            </div>
-            <p className="text 2x1 font-bold mt-4">Page Not Found</p>
-            </div>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <div className="flex-grow flex items-center justify-center bg-white">
+        <img src="/404.jpg" alt="Not Found" className=" h-80 object-contain" />
+      </div>
+    </Layout>
+  );
 };
 
 export { NotFound };
